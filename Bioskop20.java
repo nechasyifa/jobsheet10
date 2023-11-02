f2e4eb430d47da54da1dc3408b9ba07b7ff1f365
Your Name <you@example.com>
2023-11-03 00:02:23 +0700

    jobsheet10

--- Bioskop20.java
+++ Bioskop20.java
@@ -13,10 +13,10 @@ public class Bioskop20 {
         penonton[3][1] = "Hana";
 
         System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton.length);
+        }
 
     }   
 }
